8e0d1c33adc7fc6a4a09aa50666bfdfc390270e6
Matt Neary <neary.matt@gmail.com>
2013-08-08 19:52:50 -0400

    recursion by combinators and let* for prelude creation

--- interpreter/lambda.scm
+++ interpreter/lambda.scm
@@ -14,6 +14,14 @@
 	(list 'if 'found123 (cadar conds) (cond-set (cdr conds)))) 
       (car (car conds)))))
 (defmacro (cond conds) (cond-set conds))
+(defmacro (let name val expr)
+  `((lambda (,name) expr) ,val))
+(defmacro
+  (let* vars expr)
+  (if
+    (null? vars)
+    expr
+    `((lambda (,(car (car vars))) (let* ,(cdr vars) ,expr)) ,(cadar vars))))
 (define (assocv key hash) (cadr (assoc key hash)))
 (define 
   (set key val hash)
@@ -24,86 +32,80 @@
         (list key val)
         item))
     hash) (list key val)))
-(defmacro (let name val expr) `((lambda (,name) expr) ,val))
+(defmacro (let name val expr) `((lambda (,name) ,expr) ,val))
 
 "  render a flat list with parentheses of the form 
 "" `<...>` as a nested list.
 "
-(define (eatparens expr nested before paren accum found)
+(define (Z f) (f (lambda (z) (Z f))))
+(define (eatparenser eatparens) (lambda (expr nested before paren accum found)
   (if
     (null? expr)
     (if
       (not (null? paren))
-      (concat (push before paren) (parens accum))
+      (concat (push before paren) ((eatparens '()) accum 0 '() '() '() #f))
       accum)
     (cond (((and (eqv? (car expr) '<) (not found))
             (if
               (eqv? nested 0)
-              (eatparens (cdr expr) (+ nested 1) accum paren '() found)
-              (eatparens (cdr expr) (+ nested 1) before paren (push accum (car expr)) found))) 
+              ((eatparens '()) (cdr expr) (+ nested 1) accum paren '() found)
+              ((eatparens '()) (cdr expr) (+ nested 1) before paren (push accum (car expr)) found))) 
            ((and (eqv? (car expr) '>) (not found))
 	        (if
               (eqv? nested 1)
-              (eatparens (cdr expr) (- nested 1) before (parens accum) '() #t)
-              (eatparens (cdr expr) (- nested 1) before paren (push accum (car expr)) found)))
-           (#t (eatparens (cdr expr) nested before paren (push accum (car expr)) found))))))
+              ((eatparens '()) (cdr expr) (- nested 1) before (parens accum) '() #t)
+              ((eatparens '()) (cdr expr) (- nested 1) before paren (push accum (car expr)) found)))
+           (#t ((eatparens '()) (cdr expr) nested before paren (push accum (car expr)) found)))))))
+(define eatparens (Z eatparenser))           
 (define (parens expr)
   (eatparens expr 0 '() '() '() #f))
   
 " apply a list of arguments to a function  
 "
-(define (apply-set fn args)
+(define (apply-setter apply-set) (lambda (fn args)
   (if
     (null? args)
     fn
-    (apply-set (fn (car args)) (cdr args))))
+    ((apply-set '()) (fn (car args)) (cdr args)))))
+(define apply-set (Z apply-setter))    
     
 "  evaluate the lambda calculus
 "" lazy evaluation is achieved by use of lambda wrappers
 "" and then application of `'()` upon retrieval of values.
+"" recursion is achieved rather explicitly by *unwrapping*
+"" a lambda wrapped version of oneself prior to execution.
 "
-(define (eval expr env)
+(define (evaler eval) (lambda (expr env)
   (cond (((atom? expr) ((assocv expr env) '()))
          ((eqv? 'lam (car expr)) 
-	      (lambda (x) (eval (cddr expr) (set (cadr expr) x env))))
-	     ((null? (cdr expr)) (eval (car expr) env))
+	      (lambda (x) ((eval '()) (cddr expr) (set (cadr expr) x env))))
+	     ((null? (cdr expr)) ((eval '()) (car expr) env))
 	     (#t 
 	      (apply-set 
-	        (eval (car expr) env) 
+	        ((eval '()) (car expr) env) 
 	        (map 
-	          (lambda (expr) (eval (list 'lam 'z expr) env)) 
-	          (cdr expr)))))))  
+	          (lambda (expr) ((eval '()) (list 'lam 'z expr) env)) 
+	          (cdr expr))))))))  
+	          
+(define eval (Z evaler))	          
 (define (evalpar expr env)
   (eval (parens expr) env))	 
   
 " provide basic functions in a prelude
 "  
-(define prelude 
-  `((id ,(lambda(z) (lambda (x) x))) (1 ,(lambda (z) 1))))
-(define prelude 
-  (set 'true (evalpar '(lam z lam a lam b < a > id) prelude) prelude))
-(define prelude 
-  (set 'false (evalpar '(lam z lam a lam b < b > id) prelude) prelude))
-(define prelude 
-  (set 'cons (evalpar '(lam z lam a lam b lam c lam n < < c > a > b) prelude) prelude))
-(define prelude 
-  (set 'nil (evalpar '(lam z lam c lam n < n > id) prelude) prelude))
-(define prelude 
-  (set 'car (evalpar '(lam z lam l < l < lam a lam b a > id >) prelude) prelude))
-(define prelude 
-  (set 'cdr (evalpar '(lam z lam l < l < lam a lam b b > id >) prelude) prelude))
-(define prelude
-  (set 'null? (evalpar '(lam z lam l < l < lam a lam b false > < lam n true > >) prelude) prelude))
-(define prelude
-  (set 'if (evalpar '(lam z lam p lam t lam f < p t f >) prelude) prelude))
-(define prelude 
-  (set 'tuple (evalpar '(lam z lam a lam b < cons a < cons b nil > >) prelude) prelude))
-
-" a display of successful recursion
-"
-(define prelude 
-  (set 'cursor (evalpar '(lam z lam me lam n < if < null? n > < lam z 1 > < lam z < me nil > > >) prelude) prelude))
-(define prelude 
-  (set 'Y (evalpar '(lam z lam f < lam x < f < x x > > > < lam x < f < x x > > >) prelude) prelude))
-(define prelude 
-  (set 'fixed (evalpar '(lam z < Y cursor >) prelude) prelude))
+(define preludefinal '())
+(let* ((prelude `((id ,(lambda(z) (lambda (x) x))) (1 ,(lambda (z) 1))))
+       (prelude (set 'true (evalpar '(lam z lam a lam b < a > id) prelude) prelude))
+       (prelude (set 'false (evalpar '(lam z lam a lam b < b > id) prelude) prelude))
+       (prelude (set 'cons (evalpar '(lam z lam a lam b lam c lam n < < c > a > b) prelude) prelude))
+       (prelude (set 'nil (evalpar '(lam z lam c lam n < n > id) prelude) prelude))
+       (prelude (set 'car (evalpar '(lam z lam l < l < lam a lam b a > id >) prelude) prelude))
+       (prelude (set 'cdr (evalpar '(lam z lam l < l < lam a lam b b > id >) prelude) prelude))
+       (prelude (set 'null? (evalpar '(lam z lam l < l < lam a lam b false > < lam n true > >) prelude) prelude))
+       (prelude (set 'if (evalpar '(lam z lam p lam t lam f < p t f >) prelude) prelude))
+       (prelude (set 'tuple (evalpar '(lam z lam a lam b < cons a < cons b nil > >) prelude) prelude))
+       (prelude (set 'cursor (evalpar '(lam z lam me lam n < if < null? n > < lam z 1 > < lam z < me nil > > >) prelude) prelude))
+       (prelude (set 'Y (evalpar '(lam z lam f < lam x < f < x x > > > < lam x < f < x x > > >) prelude) prelude))
+       (prelude (set 'fixed (evalpar '(lam z < Y cursor >) prelude) prelude)))
+  (set! preludefinal prelude))
+(define prelude preludefinal)  
